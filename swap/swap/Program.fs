910f783f3c86faf3610ece5e8b2f90b4c540db7d
amin sheikhnia <ashei012@fiu.edu>
2019-05-14 16:11:22 -0400

    Made my own swap function. Tested it with many inputs. No problem, only syntax issues.

---
+++ swap/swap/Program.fs
@@ -0,0 +1,16 @@
+﻿let swap (a,b) = (b,a)
+
+let s = 
+    swap (10, 10.5)
+let s2 =
+    swap ("Hello", 50)
+let s3 =
+    swap ((10,"l",2.2222), "k")
+let s4 =
+    swap (("Goodbye",[10.2;52.2;892.2],-98), 2)
+
+open System
+printfn "Swap 10 and 10.5: %A" s
+printfn "Swap Hello and 50: %A" s2
+printfn "Swap (10,l,2.2222) and k: %A" s3
+printfn "Swap (Goodbye,[10.2;52.2;892.2],-98) and 2: %A" s4
