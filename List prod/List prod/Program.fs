a12d366ac7b79bc2065f8e668fd1a4786bd92e86
amin sheikhnia <ashei012@fiu.edu>
2019-05-14 12:33:32 -0400

    Started with multiplying all the items in a list. I then moved on to other operations. I got addition to work, but division and subtraction do not work. For some reason division divides in reverse and subtraction subtracts the first 2 numbers then it starts adding the rest.

---
+++ List prod/List prod/Program.fs
@@ -0,0 +1,31 @@
+﻿let rec prod ns =
+    if List.isEmpty ns then 1
+    else List.head ns * prod (List.tail ns)
+
+let rec div ms =
+    if List.isEmpty ms then 1.0
+    else List.head ms / div (List.tail ms)
+
+let rec add os =
+    if List.isEmpty os then 0
+    else List.head os + add (List.tail os)
+
+let rec sub ps =
+    if List.isEmpty ps then 0
+    else List.head ps - sub (List.tail ps)
+
+open System
+
+let a = 
+    prod [1..3]
+let b = 
+    div [1.0..3.0]
+let c = 
+    add [1..3]
+let d = 
+    sub [1..3]
+
+printfn "Product of 1 to 3 inclusive is: %A" a
+printfn "Division of 1 to 3 inclusive is: %A" b
+printfn "Addition of 1 to 3 inclusive is: %A" c
+printfn "Subtraction of 1 to 3 inclusive is: %A" d
