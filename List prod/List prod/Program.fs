703ebecfe8411a317c66e1179bea89f99220f85a
amin sheikhnia <ashei012@fiu.edu>
2019-05-14 15:26:46 -0400

    Figured out how to do sub, i had the head subtract the add (List.tail)  as a round about way to do sub. In class you explained another way to do sub by using an auxilary function. Though it does not print the answer the logic makes sense.

--- List prod/List prod/Program.fs
+++ List prod/List prod/Program.fs
@@ -10,22 +10,33 @@ let rec add os =
     if List.isEmpty os then 0
     else List.head os + add (List.tail os)
 
+let rec sub_help ans = function
+| [] -> ans
+| x::xs -> sub_help (ans - x) xs
+
+let sub_new list = function
+| [] -> 0
+| x::xs -> sub_help x xs
+
 let rec sub ps =
     if List.isEmpty ps then 0
-    else sub (List.tail ps) - List.head ps 
+    else List.head ps - add (List.tail ps)
 
 open System
 
 let a = 
-    prod [1..3]
+    prod [1..10]
 let b = 
     div [1.0..10.0]
 let c = 
-    add [1..3]
+    add [1..10]
 let d = 
-    sub [10;5;5;5]
+    sub [1..10]
+let e = 
+    sub_new [1..10]
 
-printfn "Product of 1 to 3 inclusive is: %A" a
-printfn "Division of 1 to 3 inclusive is: %A" b
-printfn "Addition of 1 to 3 inclusive is: %A" c
-printfn "Subtraction of 1 to 3 inclusive is: %A" d
+printfn "Product of 1 to 10 inclusive is: %A" a
+printfn "Division of 1 to 10 inclusive is: %A" b
+printfn "Addition of 1 to 10 inclusive is: %A" c
+printfn "Subtraction of 1 to 10 inclusive is: %A" d
+printfn "Subtraction of 1 to 10 inclusive is: %A" e
