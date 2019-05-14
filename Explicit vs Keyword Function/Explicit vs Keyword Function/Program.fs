c271850e9a2909d1d9de0a6607905d56c2dd88f1
amin sheikhnia <ashei012@fiu.edu>
2019-05-14 16:58:39 -0400

    "Side by side" view of explicit vs keyword function matching. Though both do the same thing and give the same result, the keyword function matching is more pleasing to look at.

---
+++ Explicit vs Keyword Function/Explicit vs Keyword Function/Program.fs
@@ -0,0 +1,18 @@
+﻿let rec prod = function
+| [] -> 1
+| x::xs -> x * prod xs
+
+let rec xprod xp =
+    match xp with
+    | [] -> 1
+    | n::ns -> n * xprod ns
+
+let p =
+    prod [100;5;2]
+
+let xp =
+    xprod [100;5;2]
+
+open System
+printfn "%i" p
+printfn "%i" xp
