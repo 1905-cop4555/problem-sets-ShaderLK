4b7d817f27dcfb2d0cfc893fbf1bdf12acfca808
amin sheikhnia <ashei012@fiu.edu>
2019-05-14 10:48:07 -0400

    Testing out list.map from the notes. I made it such that a was the original answer and b squares the whole list. Did not know where to place the paranthesis till i realized it squared the first list because the @ stopped the squaring so i put the lists into a paranthesis and i got both lists squared.

---
+++ List.map/List.map/Program.fs
@@ -0,0 +1,9 @@
+﻿let a =
+    List.map (fun n -> n*n) [1;2;3]@[4;5;6]
+
+let b =
+    List.map (fun m -> m*m) ([1;2;3]@[4;5;6])
+
+open System
+printfn "a = %A" a
+printfn "b = %A" b
---
+++ Problem set 18/Problem set 18/Program.fs
@@ -0,0 +1,8 @@
+﻿let rec interleave = function
+| [],[] -> []
+| [x],[y] -> [x;y]
+ 
+let a = 
+    interleave ([1;2;3],[4;5;6])
+open System
+printf "a = %A" a
