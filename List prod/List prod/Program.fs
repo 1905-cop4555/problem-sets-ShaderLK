74c6afc07d4564ec7a730020b12dbd325342d220
amin sheikhnia <ashei012@fiu.edu>
2019-05-14 12:56:01 -0400

    After fiddling with the div and sub functions I figured something out. I flipped the tail and the heads of both functuons. This worked for div, but for it did not work for sub.

--- List prod/List prod/Program.fs
+++ List prod/List prod/Program.fs
@@ -4,7 +4,7 @@
 
 let rec div ms =
     if List.isEmpty ms then 1.0
-    else List.head ms / div (List.tail ms)
+    else div (List.tail ms) / List.head ms 
 
 let rec add os =
     if List.isEmpty os then 0
@@ -12,18 +12,18 @@ let rec add os =
 
 let rec sub ps =
     if List.isEmpty ps then 0
-    else List.head ps - sub (List.tail ps)
+    else sub (List.tail ps) - List.head ps 
 
 open System
 
 let a = 
     prod [1..3]
 let b = 
-    div [1.0..3.0]
+    div [1.0..10.0]
 let c = 
     add [1..3]
 let d = 
-    sub [1..3]
+    sub [10;5;5;5]
 
 printfn "Product of 1 to 3 inclusive is: %A" a
 printfn "Division of 1 to 3 inclusive is: %A" b
