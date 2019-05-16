c9d6742c0227a0d78bb00b398f311ed56f50606d
amin sheikhnia <ashei012@fiu.edu>
2019-05-16 08:04:16 -0400

    Tested out problem set question 1 to make sure that i was correct in my assumption c is the answer.

---
+++ Problem Set 1/Problem Set 1/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+//C won't work because f# cannot compute a float and an int like in java.
+let a1 = 
+    2+5*10
+printfn "%i" a1
+let b1 = 
+    10I * 20I
+printfn "%A" b1
+//let c1 = 
+   // 4 + 5.6
+//printfn "%A" c1
+let d1 = 
+    "4" + "5.6"
+printfn "%A" d1
