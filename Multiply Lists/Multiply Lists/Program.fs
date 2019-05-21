0bd540c6f1dddd6d4ace5f1c6bc63ff607b2db8a
amin sheikhnia <ashei012@fiu.edu>
2019-05-21 11:01:50 -0400

    The second function is the correct answer to number 22 of the exam. I had this 70% correct, but I was not able to figure out how to not let the function multiply by 0. The solution was simple and I should have seen it.

---
+++ Multiply Lists/Multiply Lists/Program.fs
@@ -0,0 +1,21 @@
+﻿let rec mul a =
+    match a with 
+    |[] -> 0
+    |x::xs -> x * mul xs
+
+let m =
+    mul [1..10]
+
+printfn "%A" m
+
+
+let rec mult a=
+    match a with 
+    |[] -> 0
+    |[x] -> x
+    |x::xs -> x * mult xs
+
+let t =
+    mult [1..10]
+
+printfn "%A" t
