e4f7d776604fa5d06f3acbbfc1449017cef10b62
amin sheikhnia <ashei012@fiu.edu>
2019-05-21 10:28:03 -0400

    Tried remaking the interleave function we made in class, but the let has an error.

---
+++ Interleave/Interleave/Program.fs
@@ -0,0 +1,9 @@
+﻿let rec interleave = function
+| [],[] -> []
+| [x],[y] -> [x;y]
+| x::xs, y::ys -> let small = interleave(xs,ys) 
+                                x::y::small
+| _,_ -> failwith "not equal"
+
+
+                       
---
+++ Testing/Testing/Program.fs
@@ -0,0 +1,9 @@
+﻿let (a:float) =
+    (2.0**2.0)**3.0
+
+let (b:float) = 
+    2.0**(2.0**3.0)
+
+printfn "%f" a
+printfn "%f" b
+
